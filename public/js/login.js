b2237306650afe403ad0c3ed21ce9d6e4bed8895
avilwock <151412286+avilwock@users.noreply.github.com>
2024-04-03 09:06:34 -0700

    updated login to redirect to dash upon finish

--- public/js/login.js
+++ public/js/login.js
@@ -18,7 +18,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dash');
     } else {
       alert('Failed to log in');
     }
